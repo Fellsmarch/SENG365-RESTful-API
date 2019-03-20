216fd47c8ea1acfd42d060bd36d8e2d447173214
hgc25 <hgc25@uclive.ac.nz>
2019-03-20 23:01:11 +1300

    Completed GET /venues/:venueId/reviews

--- app/controllers/venue.review.controller.js
+++ app/controllers/venue.review.controller.js
@@ -9,11 +9,23 @@ exports.getVenueReviews = function(req, resp) {
         if (!result) {
             resp.json(response.message);
         } else {
-
-            //TODO: Map result to correct json parameters
+            let toSend = [];
+            for (let i = 0; i < result.length; i++) {
+                let row = result[i];
+                let newObject = {
+                    "reviewAuthor": {
+                        "userId": row["user_id"],
+                        "username": row["username"]},
+                    "reviewBody": row["review_body"],
+                    "starRating": row["star_rating"],
+                    "costRating": row["cost_rating"],
+                    "timePosted": row["time_posted"]
+                };
+                toSend.push(newObject);
+            }
+            resp.json(toSend);
         }
     });
-    resp.send();
 };
 
 exports.addReview = function(req, resp) {
--- app/models/venue.review.model.js
+++ app/models/venue.review.model.js
@@ -2,13 +2,14 @@ const db = require("../../config/db");
 const responses = require("../util/util.responses");
 
 exports.getManyByVenueId = function(venueId, done) {
-    let query = "SELECT user_id, username, review_body, star_rating, cost_rating, time_posted FROM Review" +
-        "JOIN User ON user_id = review_author_id" +
-        "WHERE reviewed_venue_id = ?";
+    let query = "SELECT user_id, username, review_body, star_rating, cost_rating, time_posted FROM Review " +
+        "JOIN User ON user_id = review_author_id " +
+        "WHERE reviewed_venue_id = ? " +
+        "ORDER BY time_posted DESC";
 
     db.getPool().query(query, venueId, function(err, rows) {
        if (err) {
-           console.log("REVIEWS GET MAN BY VENUE ID ERROR:\n" + err);
+           console.log("REVIEWS GET MANY BY VENUE ID ERROR:\n" + err);
            done(null, responses._500);
        } else if (rows.length < 1) {
            done(null, responses._404);
